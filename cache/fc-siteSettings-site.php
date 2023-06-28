e472d0fde86874f1030651191f3daff6fdeb1c34
diegofalconi96 <diego.falconi4@utc.edu.ec>
2023-06-28 18:21:07 +0000

    Carga Inicial

---
+++ cache/fc-siteSettings-site.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'contactEmail' => 
+  array (
+    'en_US' => 'diego.falconi4@utc.edu.ec',
+  ),
+  'contactName' => 
+  array (
+    'en_US' => 'Open Journal Systems',
+  ),
+  'showDescription' => true,
+  'showThumbnail' => true,
+  'showTitle' => true,
+  'themePluginPath' => 'default',
+); ?>
